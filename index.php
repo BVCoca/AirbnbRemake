8881dc8f7cba25c072a9b043ad88c4b620d001b9
Benjamin Vaique <vaique.benjamin@gmail.com>
2023-06-15 16:32:35 +0200

    Ajout de la base du projet
    Co-authored-by: PimentelAndre <PimentelAndre@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php require_once 'common/header.php' ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
 
-<body>
-
-</body>
-
-</html>
+<?php require_once 'common/footer.php' ?>   
