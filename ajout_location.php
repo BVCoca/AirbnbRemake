d7bd3a357e1f5a07acedb0d94b85305d4fa3b7f5
Benjamin Vaique <vaique.benjamin@gmail.com>
2023-06-15 16:59:47 +0200

    ...

--- ajout_location.php
+++ ajout_location.php
@@ -2,10 +2,13 @@
 require_once 'inc/init.php';
 
 // Redirection de l'utilisateur si il n'est pas connecté à la page de connexion.
+// Mis en commentaire en attente de la création de la page connexion.
 
+/* 
 if (!isLogged()) {
     header('Location: connexion.php');
 }
+ */
 
 $errors = [];
 
@@ -13,6 +16,7 @@ $showMessage = '';
 ?>
 
 <?php require_once 'common/header.php'; ?>
-
+<!-- Titre pour savoir que c'est la page ajout_location.php -->
+<h1>Ajouter une location</h1>
 
 <?php require_once 'common/footer.php'; ?>
