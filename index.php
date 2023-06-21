b7144ce9d75e4707ae049bb1587722dfbf358faa
Benjamin Vaique <vaique.benjamin@gmail.com>
2023-06-21 11:35:53 +0200

    Initialisation des filtres dans l'index

--- index.php
+++ index.php
@@ -1,6 +1,81 @@
 <?php require_once 'common/header.php'; ?>
-<?php require_once 'inc/init.php'; ?>
-<?php linkResource("stylesheet", "common/style.css"); ?>
+<?php require_once 'inc/init.php';
+linkResource("stylesheet", "common/style.css");
+
+
+$req = $db->prepare('SELECT DISTINCT (filtre) FROM location');
+$req->execute();
+$content = '';
+
+
+/* while ($filtres = $req->fetch(PDO::FETCH_ASSOC)) {
+    $content .= "<a href='?filtre=" . $filtres['filtre'] . "'>" . $filtres['filtre'] . "</a><br>";
+}
+
+if (isset($_GET['filtre'])) {
+    $req1 = $db->prepare('SELECT * FROM location WHERE filtre = :filtre');
+    $req1->bindValue(':filtre', $_GET['filtre'], PDO::PARAM_STR);
+    $req1->execute();
+
+    if ($req1->rowCount() <= 0) {
+        header('location: index.php');
+        exit();
+    }
+
+    while ($locations = $req1->fetch(PDO::FETCH_ASSOC)) {
+        if (is_array($locations)) {
+            $locationDebut = afficherDateEnFrancais($locations['date_debut']);
+            $locationFin = afficherDateEnFrancais($locations['date_fin']);
+
+            $img = $db->prepare('SELECT `imgName` FROM `image` WHERE id_location = :id_location');
+            $img->bindValue(':id_location', $locations['id'], PDO::PARAM_INT);
+            $img->execute();
+
+            $content .= '<div class="card my-2 img-thumbnail" style="width: 18rem;">';
+            $content .= '<img src="' . URL . $img['imgName'] . '" class="card-img-top" alt="...">';
+            $content .= '<div class="card-body">';
+            $content .= '<h5 class="card-title">' . $locations['titre'] . '</h5>';
+            $content .= '<p class="card-text">' . substringsfn($locations['description'], 50) . '</p>';
+            $content .= '<p class="card-text">' . $locations['prix'] . ' €</p>';
+            $content .= '<p class="card-text">' . $locations['ville'] . '</p>';
+            $content .= '<p class="card-text">' . $locations['code_postal'] . '</p>';
+            $content .= '<p class="card-text">' . $locationDebut . ' - ' . $locationFin . '</p>';
+            $content .= '<a href="detail_location.php?id=' . $locations['id'] . '" class="btn btn-primary">Voir la location</a>';
+            $content .= '</div>';
+            $content .= '</div>';
+        }
+    }
+} else {
+    $req2 = $db->prepare('SELECT * FROM article');
+    $req2->execute();
+
+    while ($locations = $req2->fetch(PDO::FETCH_ASSOC)) {
+        if (is_array($locations)) {
+            $locationDebut = afficherDateEnFrancais($locations['date_debut']);
+            $locationFin = afficherDateEnFrancais($locations['date_fin']);
+
+            $img = $db->prepare('SELECT `imgName` FROM `image` WHERE id_location = :id_location');
+            $img->bindValue(':id_location', $locations['id'], PDO::PARAM_INT);
+            $img->execute();
+
+            $content .= '<div class="card my-2 img-thumbnail" style="width: 18rem;">';
+            $content .= '<img src="' . URL . $img['imgName'] . '" class="card-img-top" alt="...">';
+            $content .= '<div class="card-body">';
+            $content .= '<h5 class="card-title">' . $locations['titre'] . '</h5>';
+            $content .= '<p class="card-text">' . substringsfn($locations['description'], 50) . '</p>';
+            $content .= '<p class="card-text">' . $locations['prix'] . ' €</p>';
+            $content .= '<p class="card-text">' . $locations['ville'] . '</p>';
+            $content .= '<p class="card-text">' . $locations['code_postal'] . '</p>';
+            $content .= '<p class="card-text">' . $locationDebut . ' - ' . $locationFin . '</p>';
+            $content .= '<a href="detail_location.php?id=' . $locations['id'] . '" class="btn btn-primary">Voir la location</a>';
+            $content .= '</div>';
+            $content .= '</div>';
+        }
+    }
+}
+ */
+
+?>
 
 <div class="container">
     <div class="row text-center">
@@ -11,14 +86,19 @@
             Nos dernières locations :
         </h3>
     </div>
+
+
     <div class="row d-flex justify-content-between">
+        <?php echo $content; ?>
         <?php
         $data = $db->prepare('SELECT * FROM `location` ORDER BY titre DESC');
         $data->execute();
         while ($location = $data->fetch(PDO::FETCH_ASSOC)) {
-            $locationDebut = afficherDateEnFrancais($location['date_debut']);
-            $locationFin = afficherDateEnFrancais($location['date_fin']);
+
+
             if (is_array($location)) {
+                $locationDebut = afficherDateEnFrancais($location['date_debut']);
+                $locationFin = afficherDateEnFrancais($location['date_fin']);
                 $author = $db->prepare('SELECT `id`, `nom`, `prenom`, `password`, `email`, `telephone` FROM `users` WHERE id = :id_user');
                 $author->bindValue(':id_user', $location['id'], PDO::PARAM_INT);
                 $author->execute();
