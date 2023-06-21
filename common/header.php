8ca4ed03221af6acdcc155e652a27cf9ad5816fa
andré pimentel <andrepiimentel@gmail.com>
2023-06-21 12:07:44 +0200

    Style

--- common/header.php
+++ common/header.php
@@ -13,6 +13,10 @@
     <title>Airbnb</title>
     <link rel="stylesheet" href="style.css">
     <link rel="icon" href="img/favicon-16x16.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400&display=swap" rel="stylesheet">
 </head>
 
 <body>
